2cfa6c3adaa19d56616365a9aa890e66931587d2
j1kk <armyovhades@gmail.com>
2020-08-03 12:44:19 +0800

    add javascript101

---
+++ 08_Including_Scripts/JS_stalker_exercise.js
@@ -0,0 +1,10 @@
+var userFname = prompt("What is your first name?");
+var userLname = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+
+// alternative 
+// var fullname = userFname + " " + userLname
+// console.log("your full name is " + fullname);
+
+console.log("your full name is " + userFname + " " +userLname);
+console.log("you are " + userAge + " years old");
